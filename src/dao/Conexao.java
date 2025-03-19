4496b9ac78f33a893ceaf530ff1f3d90d6216fcf
rafael.garcia <Rafael.garcia21@outlook.com>
2025-03-19 09:39:34 -0300

    Remover credenciais sensíveis do código

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -1,16 +1,16 @@
 package dao;
 
-
-
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    // URL ajustada com o nome do banco, porta e sslMode=REQUIRED
-    private static final String URL = "jdbc:mysql://finaciaitext-rafaelgarcia86-f678.g.aivencloud.com:21130/defaultdb?sslMode=REQUIRED";
-    private static final String USUARIO = "avnadmin";
-    private static final String SENHA = "AVNS_UehnzHsZwgGsPNb0Cuq";
+    private static Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USUARIO = dotenv.get("DB_USER");
+    private static final String SENHA = dotenv.get("DB_PASSWORD");
 
     public static Connection conectar() {
         try {
